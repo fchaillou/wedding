735c8252fffa8197accc7fa254f469fe807fde39
Fabien Chaillou <fabien.chaillou@gmail.com>
2013-12-07 12:52:57 -0500

    Initial commit of the project

---
+++ app/controllers/MainController.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+object MainController extends Controller {
+
+  def index = Action {
+    Ok(views.html.index.render())
+  }
+
+
+}
