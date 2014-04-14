54720c03fc9d57f58dca2fc6f4418cbb5e7b396f
Fabien Chaillou <fabien.chaillou@gmail.com>
2014-04-13 20:36:07 -0400

    Ajout de page pour chaque section

--- app/controllers/MainController.scala
+++ app/controllers/MainController.scala
@@ -8,5 +8,42 @@ object MainController extends Controller {
     Ok(views.html.index.render())
   }
 
+  def story  = Action {
+    Ok(views.html.story.render())
+  }
+
+  def cityhall  = Action {
+    Ok(views.html.cityhall.render())
+  }
+
+  def church  = Action {
+    Ok(views.html.church.render())
+  }
+
+  def cocktail  = Action {
+    Ok(views.html.cocktail.render())
+  }
+
+  def food  = Action {
+    Ok(views.html.food.render())
+  }
+
+  def party  = Action {
+    Ok(views.html.party.render())
+  }
+
+  def hotels  = Action {
+    Ok(views.html.hotels.render())
+  }
+
+  def brunch  = Action {
+    Ok(views.html.brunch.render())
+  }
+
+  def honeymoon  = Action {
+    Ok(views.html.honeymoon.render())
+  }
+
+
 
 }
